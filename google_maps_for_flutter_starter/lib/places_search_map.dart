71fb3b23985cc12aee327df6b679dba3dd757679
gyan0890 <gyanlakshmi@gmail.com>
2020-03-22 20:02:53 +0530

    Flutter app for google maps tracker
    ActivityTracker Flutter Repo
    
    Co-Authored-By: saikat27 <saikat27@users.noreply.github.com>

---
+++ google_maps_for_flutter_starter/lib/places_search_map.dart
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2019 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+import 'dart:async';
+import 'dart:convert';
+import 'data/error.dart';
+import 'data/place_response.dart';
+import 'data/result.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+class PlacesSearchMapSample extends StatefulWidget {
+  final String keyword;
+  PlacesSearchMapSample(this.keyword);
+
+  @override
+  State<PlacesSearchMapSample> createState() {
+    return _PlacesSearchMapSample();
+  }
+}
+
+class _PlacesSearchMapSample extends State<PlacesSearchMapSample> {
+  static const String _API_KEY = '{{YOU_API_KEY_HERE}}';
+
+  static double latitude = 40.7484405;
+  static double longitude = -73.9878531;
+  static const String baseUrl =
+      "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
+
+  
+  Error error;
+  List<Result> places;
+  bool searching = true;
+  String keyword;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+  void _handleResponse(data){
+    // bad api key or otherwise
+      if (data['status'] == "REQUEST_DENIED") {
+        setState(() {
+          error = Error.fromJson(data);
+        });
+        // success
+      } else if (data['status'] == "OK") {
+        
+      } else {
+        print(data);
+      }
+  }
+}
